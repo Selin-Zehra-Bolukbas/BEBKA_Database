d2df3d3eb3cc4ca355c990e82bb5b374365e231c
PC <selinz.bolukbas@gmail.com>
2025-07-16 15:06:51 +0300

    [JA-5] - PreparedStatement ile Veri Ekle

--- database_process/src/main/java/com/selin/Main.java
+++ database_process/src/main/java/com/selin/Main.java
@@ -5,10 +5,12 @@ import com.selin.config.DataBaseConfig;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
+
 
         String sql="CREATE TABLE IF NOT EXISTS users (" +
                 "id SERIAL PRIMARY KEY," +
@@ -17,12 +19,25 @@ public class Main {
 
         try {
             Connection connection= DriverManager.getConnection(DataBaseConfig.DATABASE_URL, DataBaseConfig.DATABASE_USERNAME,DataBaseConfig.DATABASE_PASSWORD);
-            Statement statement = connection.createStatement();
+           Statement statement = connection.createStatement();
             statement.execute(sql);
             System.out.println("Tablo oluşturuldu.");
+
+
+            String insertsql= "INSERT INTO users(name,email) VALUES(?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(insertsql);
+            preparedStatement.setString(1,"Ali");
+            preparedStatement.setString(2,"ali@gmail.com");
+            preparedStatement.executeUpdate();
+
         }catch (Exception e){
             e.printStackTrace();
         }
 
+
+
+
     }
+
+
 }
